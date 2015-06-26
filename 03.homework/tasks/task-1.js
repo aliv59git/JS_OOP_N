be0c2b97f8a399a23179283d75bedda8bfceab6b
aliv59git <vilavaln@gmail.com>
2015-06-26 16:01:02 +0300

    new homew

--- 03.homework/tasks/task-1.js
+++ 03.homework/tasks/task-1.js
@@ -17,9 +17,55 @@
 */
 function solve() {
 	var Person = (function () {
-		function Person() {
+    var minSymbols = 3,
+        maxSymbols = 20;
+
+    var isValidName = function(val){
+      if(val.length < minSymbols || val.length > maxSymbols){
+        throw new Error('Names length muist be beween '+ minSymbols + ' and '+ maxSymbols + ' symbols!');
+      }
+      if(!/^[A-Za-z]/.test(val)){
+        throw new Error('Names must containing symbls A-Z, a-z!')
+      }
+    }
+
+    var isValidAge = function(val){
+      if(isNaN(val) || val < 0 || val > 150){
+        throw new Error('Age must always be a number in the range 0 - 150!');
+      }
+    }
+
+		function Person(fname, lname, age) {
+      if(!(this instanceof arguments.callee)){
+        return new Person(fname, lname, age);
+      }
+      isValidName(fname);
+      isValidName(lname);
+      isValidAge(age);
+
+      this.firstname = fname;
+      this.lastname = lname;
+      this.age = age;
 		}
-		
+
+    Object.defineProperty(Person.prototype, 'fullname', {
+      get: function(){
+        return this.firstname + ' ' + this.lastname;
+      },
+      set: function(value){
+        var names = value.split(' ');
+        this.firstname = names[0];
+        this.lastname = names[1];
+        if(isValidName(this.firstname) && isValidName(this.lastname)){
+          this.fullname = value;
+        }
+      }
+    });
+
+    Person.prototype.introduce = function(){
+       return 'Hello! My name is '+ this.fullname + ' and I am ' + this.age + '-years-old';
+    }
+
 		return Person;
 	} ());
 	return Person;
